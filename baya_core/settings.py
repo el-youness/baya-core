aebe4d05719746955c816ce3ad3cd5c3afa9d066
el-youness <youness.elidrissi@outlook.fr>
2023-12-06 00:21:03 +0100

    fix settings.py

--- baya_core/settings.py
+++ baya_core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +139,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# add these lines at end
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+django_on_heroku.settings(locals())
--- settings.py
+++
@@ -1,3 +0,0 @@
-# Configure Django App for Heroku.
-import django_on_heroku
-django_on_heroku.settings(locals())
